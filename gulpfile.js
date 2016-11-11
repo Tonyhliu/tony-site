a11c9aaa3f04c42bfeb9957b3c125b45f50ad55d
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-11-11 12:21:18 -0800

    renamed vendor folder

--- gulpfile.js
+++ gulpfile.js
@@ -54,31 +54,31 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy Bootstrap core files from node_modules to vendor directory
+// Copy Bootstrap core files from node_modules to thirdparty directory
 gulp.task('bootstrap', function() {
     return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('thirdparty/bootstrap'))
 })
 
-// Copy jQuery core files from node_modules to vendor directory
+// Copy jQuery core files from node_modules to thirdparty directory
 gulp.task('jquery', function() {
     return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('thirdparty/jquery'))
 })
 
-// Copy Magnific Popup core files from node_modules to vendor directory
+// Copy Magnific Popup core files from node_modules to thirdparty directory
 gulp.task('magnific-popup', function() {
     return gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
+        .pipe(gulp.dest('thirdparty/magnific-popup'))
 })
 
 // Copy ScrollReveal JS core JavaScript files from node_modules
 gulp.task('scrollreveal', function() {
     return gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
+        .pipe(gulp.dest('thirdparty/scrollreveal'))
 })
 
-// Copy Font Awesome core files from node_modules to vendor directory
+// Copy Font Awesome core files from node_modules to thirdparty directory
 gulp.task('fontawesome', function() {
     return gulp.src([
             'node_modules/font-awesome/**',
@@ -88,7 +88,7 @@ gulp.task('fontawesome', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('thirdparty/font-awesome'))
 })
 
 // Copy all dependencies from node_modules
